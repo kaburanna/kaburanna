8eb3f23be9a9218f80a5ca0e63804880f6ff69f5
kaburanna <116432909+kaburanna@users.noreply.github.com>
2022-11-26 19:47:03 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @kaburanna
+- 👀 I’m interested in coding
+- 🌱 I’m currently learning the bisics
+- 💞️ I’m looking to collaborate on updats on the simplifications
+- 📫 How to reach me kaburubak4018@gmail.com
+
+<!---
+kaburanna/kaburanna is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
